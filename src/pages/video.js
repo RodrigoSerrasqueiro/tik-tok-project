0b996c4b112561562d1210045a649569e1806e33
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-06-14 22:41:31 -0300

    feat(video component)

---
+++ src/pages/video.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+
+  const VideoRef = useRef(null);
+  const [play, setPlay] = useState(false)
+
+  function handleStart() {
+    if (play) {
+      VideoRef.current.pause()
+      setPlay(false) 
+    }
+    else {
+      VideoRef.current.play()
+      setPlay(true)
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        loop
+        ref={VideoRef}
+        onClick={handleStart}
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
